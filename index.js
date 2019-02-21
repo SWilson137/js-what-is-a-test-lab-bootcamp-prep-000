2811c3a9a8d2849f5167035d8193306e38aa9a74
smiling-ping-3937 <shawn.wilson137@gmail.com>
2019-02-21 19:16:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var name = "Susan"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
-console.log(parseInt(height)-25)
+console.log(parseInt(height)-35)
 
 module.exports = {name, height, message}
